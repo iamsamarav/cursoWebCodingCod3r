873d7f9724750078f2c94614d5841fce9ea803fb
Samara Vitória <samiiivits@gmail.com>
2023-03-13 16:43:36 -0300

    Learning XML

--- ajax/server.js
+++ ajax/server.js
@@ -6,7 +6,6 @@ app.use(express.static('.')) //é o provedor de todos os arquivos estáticos
 app.use(bodyParser.urlencoded({ extended: true})) //transforma o submit do form em objeto
 app.use(bodyParser.json()) //transforma dados em objetos
 
-app.get('test', (req, res) => res.send('Ok'))
-app.listen(8080, () => {
+app.listen(8081, () => {
     console.log('Executando')
 })
