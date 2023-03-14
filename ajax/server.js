a540190485794fbddb9a335f7c7f74b459f75f81
Samara Vitória <samiiivits@gmail.com>
2023-03-14 18:38:22 -0300

    XMLHttpRequest update

--- ajax/server.js
+++ ajax/server.js
@@ -6,6 +6,30 @@ app.use(express.static('.')) //é o provedor de todos os arquivos estáticos
 app.use(bodyParser.urlencoded({ extended: true})) //transforma o submit do form em objeto
 app.use(bodyParser.json()) //transforma dados em objetos
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, '.')
+    }, 
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res)=>{
+    upload(req,res,err =>{
+        if (err) {
+            return res.end('Ocorreu um erro!')
+        } 
+        
+        res.end('Concluído')
+
+        })
+    })
+
 app.listen(8081, () => {
     console.log('Executando')
 })
